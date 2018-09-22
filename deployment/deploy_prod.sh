3aeb3b2bb0d4e36f01275cc7ebe34d8ba70a4741
takundac <tkchikondo@gmail.com>
2018-09-23 01:31:13 +0200

    remove the requirements from deployment file

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -4,7 +4,6 @@ ssh root@206.189.17.132 <<EOF
   cd famouscasting
   git pull origin master
   source /opt/envs/famouscasting/bin/activate
-  pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart famouscasting
   exit
